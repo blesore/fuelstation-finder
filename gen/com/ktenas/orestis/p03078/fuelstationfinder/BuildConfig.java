147ea83b18ef21b186b1423507cee14c6a665c36
Orestis Ktenas <orestiskt@gmail.com>
2014-08-19 12:02:47 +0300

    git repo cleanup

--- gen/com/ktenas/orestis/p03078/fuelstationfinder/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ktenas.orestis.p03078.fuelstationfinder;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
